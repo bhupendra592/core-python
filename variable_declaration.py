dadd4345509fb4db0e83a761d027ec305eb6e205
bhupendra592 <bhupendra.jmd@gmail.com>
2024-06-27 20:05:37 +0530

    day 1 pythin

---
+++ variable_declaration.py
@@ -0,0 +1,19 @@
+num1 = 20
+
+name = "cdac"
+
+status = True
+
+print(type(num1))
+print(type(name))
+print(type(status))
+num2= 102
+sum = num1 + num2
+print(sum)
+print(name)
+#print(num1+name)
+
+"""
+Students are so tired that they felt 8 PM around
+7:57 PM
+"""
